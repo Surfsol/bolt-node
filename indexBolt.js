3c941c6e16b6ab8e46fbd9e0db6e615bb7579f51
Surfsol <surfsol@protonamil.com>
2020-12-30 15:14:06 -0300

    remove env

--- indexBolt.js
+++ indexBolt.js
@@ -1,56 +1,56 @@
-const { App } = require('@slack/bolt');
-require('dotenv').config()
+// const { App } = require('@slack/bolt');
+// require('dotenv').config()
 
-const app = new App({
-  signingSecret: process.env.SIGNING_SECRET,
-  token: process.env.TOKEN
-});
-console.log('in slack node app')
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-    console.log('message', message)
-    // say() sends a message to the channel where the event was triggered
-    await say({
-      blocks: [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `Hey there <@${message.user}>!`
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Click Me"
-            },
-            "action_id": "button_click"
-          }
-        }
-      ],
-      text: `Hey there <@${message.user}>!`
-    });
-  });
+// const app = new App({
+//   signingSecret: process.env.SIGNING_SECRET,
+//   token: process.env.TOKEN
+// });
+// console.log('in slack node app')
+// // Listens to incoming messages that contain "hello"
+// app.message('hello', async ({ message, say }) => {
+//     console.log('message', message)
+//     // say() sends a message to the channel where the event was triggered
+//     await say({
+//       blocks: [
+//         {
+//           "type": "section",
+//           "text": {
+//             "type": "mrkdwn",
+//             "text": `Hey there <@${message.user}>!`
+//           },
+//           "accessory": {
+//             "type": "button",
+//             "text": {
+//               "type": "plain_text",
+//               "text": "Click Me"
+//             },
+//             "action_id": "button_click"
+//           }
+//         }
+//       ],
+//       text: `Hey there <@${message.user}>!`
+//     });
+//   });
   
-  app.action('button_click', async ({ body, ack, say }) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> clicked the button`);
-  });
+//   app.action('button_click', async ({ body, ack, say }) => {
+//     // Acknowledge the action
+//     await ack();
+//     await say(`<@${body.user.id}> clicked the button`);
+//   });
 
-// Listens to incoming messages that contain "goodbye"
-app.message('goodbye', async ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    await say(`See ya later, <@${message.user}> :wave:`);
-  });
+// // Listens to incoming messages that contain "goodbye"
+// app.message('goodbye', async ({ message, say }) => {
+//     // say() sends a message to the channel where the event was triggered
+//     await say(`See ya later, <@${message.user}> :wave:`);
+//   });
 
-(async () => {
-  // Start the app
-  try {
-  await app.start(process.env.PORT || 4390);
-  console.log('⚡️ Bolt app is running!');
-  }catch(err){
-      console.log(err)
-  }
-})();
+// (async () => {
+//   // Start the app
+//   try {
+//   await app.start(process.env.PORT || 4390);
+//   console.log('⚡️ Bolt app is running!');
+//   }catch(err){
+//       console.log(err)
+//   }
+// })();
 
